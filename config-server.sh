a708b3cf75512ea9b509328d171dfab3c5970179
jpmjunior <junior.linhares@yahoo.com.br>
2023-02-17 14:15:21 -0300

    Criação de usuários otimizada

--- config-server.sh
+++ config-server.sh
@@ -1,36 +1,42 @@
 #!/bin/bash
 
-# Executa comandos como super user
+echo "Executando comandos como super usuário..."
 sudo su
 
-# Criando grupos
+echo "Criando grupos..."
 groupadd GRP_ADM
 groupadd GRP_VEN
 groupadd GRP_SEC
 
-# Criando usuários do grupo ADM
-useradd carlos -s /bin/bash -m -p $(openssl passwd -crypt senha123)
-passwd carlos -e
+echo "Criando usuários do grupo ADM..."
+useradd carlos -s /bin/bash -m -p $(openssl passwd -crypt senha123) -G GRP_ADM carlos
+useradd maria -s /bin/bash -m -p $(openssl passwd -crypt senha123) -G GRP_ADM maria
+useradd joao -s /bin/bash -m -p $(openssl passwd -crypt senha123) -G GRP_ADM joao
 
-useradd maria -s /bin/bash -m -p $(openssl passwd -crypt senha123)
-passwd maria -e
+echo "Criando usuários do grupo VEN..."
+useradd debora -s /bin/bash -m -p $(openssl passwd -crypt senha123) -G GRP_VEN debora
+useradd sebastiana -s /bin/bash -m -p $(openssl passwd -crypt senha123) -G GRP_VEN sebastiana
+useradd roberto -s /bin/bash -m -p $(openssl passwd -crypt senha123) -G GRP_VEN roberto
 
-useradd joao -s /bin/bash -m -p $(openssl passwd -crypt senha123)
-passwd joao -e
+echo "Criando usuários do grupo SEC..."
+useradd josefina -s /bin/bash -m -p $(openssl passwd -crypt senha123) -G GRP_SEC josefina
+useradd amanda -s /bin/bash -m -p $(openssl passwd -crypt senha123) -G GRP_SEC amanda
+useradd rogerio -s /bin/bash -m -p $(openssl passwd -crypt senha123) -G GRP_SEC rogerio
 
-# Criando diretórios
+echo "Criando diretórios..."
 mkdir /publico
 mkdir /adm
 mkdir /ven
 mkdir /sec
 
-# Alterando grupos dos diretórios
+echo "Alterando permissões dos diretórios..."
 chown root:GRP_ADM /adm
 chown root:GRP_VEN /ven
 chown root:GRP_SEC /sec
 
-# Alterando permissões dos diretórios
 chmod 777 /publico
 chmod 770 /adm
 chmod 770 /ven
 chmod 770 /sec
+
+echo "Final do script!"
