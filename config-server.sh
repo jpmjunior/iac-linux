abe050f84613249c4f78d9b51cfd93fc2671c4bc
jpmjunior <junior.linhares@yahoo.com.br>
2023-02-17 15:22:50 -0300

    Correção de comandos e adição de comentários

--- config-server.sh
+++ config-server.sh
@@ -1,27 +1,36 @@
 #!/bin/bash
 
-echo "Executando comandos como super usuário..."
-sudo su
+# Provisionamento de um servidor Linux com criação de usuários, grupos, permissões e diretórios 
+# para os departamentos de uma empresa:
+#
+# ADM - Administração
+# VEN - Vendas
+# SEC - Secretariado
+#
+# Obs.: Este scrict deve ser executado por um usuário com permissão de administrador.
+
+echo "Iniciando script..."
 
 echo "Criando grupos..."
 groupadd GRP_ADM
 groupadd GRP_VEN
 groupadd GRP_SEC
 
+# No OpenSSL 3.0 em diante é necessário trocar o parâmetro -crypt por -salt
 echo "Criando usuários do grupo ADM..."
-useradd carlos -s /bin/bash -m -p $(openssl passwd -crypt senha123) -G GRP_ADM carlos
-useradd maria -s /bin/bash -m -p $(openssl passwd -crypt senha123) -G GRP_ADM maria
-useradd joao -s /bin/bash -m -p $(openssl passwd -crypt senha123) -G GRP_ADM joao
+useradd carlos -s /bin/bash -m -p $(openssl passwd -crypt senha123) -G GRP_ADM
+useradd maria -s /bin/bash -m -p $(openssl passwd -crypt senha123) -G GRP_ADM
+useradd joao -s /bin/bash -m -p $(openssl passwd -crypt senha123) -G GRP_ADM
 
 echo "Criando usuários do grupo VEN..."
-useradd debora -s /bin/bash -m -p $(openssl passwd -crypt senha123) -G GRP_VEN debora
-useradd sebastiana -s /bin/bash -m -p $(openssl passwd -crypt senha123) -G GRP_VEN sebastiana
-useradd roberto -s /bin/bash -m -p $(openssl passwd -crypt senha123) -G GRP_VEN roberto
+useradd debora -s /bin/bash -m -p $(openssl passwd -crypt senha123) -G GRP_VEN
+useradd sebastiana -s /bin/bash -m -p $(openssl passwd -crypt senha123) -G GRP_VEN
+useradd roberto -s /bin/bash -m -p $(openssl passwd -crypt senha123) -G GRP_VEN
 
 echo "Criando usuários do grupo SEC..."
-useradd josefina -s /bin/bash -m -p $(openssl passwd -crypt senha123) -G GRP_SEC josefina
-useradd amanda -s /bin/bash -m -p $(openssl passwd -crypt senha123) -G GRP_SEC amanda
-useradd rogerio -s /bin/bash -m -p $(openssl passwd -crypt senha123) -G GRP_SEC rogerio
+useradd josefina -s /bin/bash -m -p $(openssl passwd -crypt senha123) -G GRP_SEC
+useradd amanda -s /bin/bash -m -p $(openssl passwd -crypt senha123) -G GRP_SEC
+useradd rogerio -s /bin/bash -m -p $(openssl passwd -crypt senha123) -G GRP_SEC
 
 echo "Criando diretórios..."
 mkdir /publico
